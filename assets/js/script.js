4f9821ea520c85bd6faee7c0e8dda3d1a7a44038
Fabiosilva112 <fabiodasilvasalustiano@gmail.com>
2023-06-20 22:34:53 -0300

    responsivo 1.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,29 @@ function menuShow() {
         document.querySelector('.icon').src="./assets/img/close_white_36dp.svg";
     }
 }
+
+// Obtém os elementos do carrossel
+const carouselWrapper = document.querySelector('.main-sectionOneTwo-divTwo-wrapper');
+const carouselCards = document.querySelectorAll('.main-sectionOneTwo-divTwo-card');
+const prevBtn = document.querySelector('.main-sectionOneTwo-prev-btn');
+const nextBtn = document.querySelector('.main-sectionOneTwo-next-btn');
+
+let currentSlide = 0;
+
+function showSlide(slideIndex) {
+  carouselWrapper.style.transform = `translateX(-${slideIndex * 280}px)`; // 310 é a largura do slide (300 + 10 de margem)
+}
+
+function prevSlide() {
+  if (currentSlide > 0) {
+    currentSlide--;
+    showSlide(currentSlide);
+  }
+}
+
+function nextSlide() {
+  if (currentSlide < carouselCards.length - 1) {
+    currentSlide++;
+    showSlide(currentSlide);
+  }
+}
